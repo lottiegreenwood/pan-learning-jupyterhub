0e7351999c2951642802db4e9675234022326704
moving-northwards <32637035+moving-northwards@users.noreply.github.com>
2021-06-15 11:28:02 +0200

    Update csvreaderformoodle.py

--- csvreaderformoodle.py
+++ csvreaderformoodle.py
@@ -9,6 +9,10 @@ jhubvaluemapping = {
   'CPU': 'REQUESTED_CPU',
   'Memory': 'REQUESTED_MEMORY',
   'Container': 'REQUESTED_CONTAINER',
+  'Software': 'REQUIRED_SOFTWARE',
+  'Containerfile': 'DOCKERFILE',
+  'Dockerhub': 'DOCKERHUB',
+  'Permission': 'PERMISSION_TO_SHARE'  
 }
 with open('surveyresponse.csv', mode='r') as infile: #or whatever the .csv file is called
   reader = csv.DictReader(infile)
@@ -16,10 +20,11 @@ with open('surveyresponse.csv', mode='r') as infile: #or whatever the .csv file 
 
 #loop using keys
 for keys in responses:
-  #strippedkey = (re.sub('[()]','', keys.split(' ', 1)[0]))
-  if "(" not in keys:
-    continue
-  strippedkey = keys.split("(")[1].split(")")[0]
+  strippedkey = (re.sub('[()]','', keys.split(' ', 1)[0]))
+  #This if statement was required when Github appeared in too separate question labels
+  #if "(" not in keys:
+  #  continue
+  #strippedkey = keys.split("(")[1].split(")")[0]
   print(strippedkey)
   if strippedkey in jhubvaluemapping: #ie should not be an empty answer
     moodlevalue = jhubvaluemapping[strippedkey]
